ea3887d98ebf756978030dd0e4badb3f482e6084
Jo Van Eyck <j.vaneyck@farmad.be>
2023-09-17 15:25:49 +0200

    c# alternative

---
+++ csharp/Bingo/BingoBoard.cs
@@ -0,0 +1,56 @@
+﻿namespace Bingo;
+
+public class BingoBoard
+{
+
+    private readonly string?[,] _cells;
+    private readonly bool[,] _marked;
+
+    public BingoBoard(int width, int height)
+    {
+        this._cells = new string[width, height];
+        this._marked = new bool[width,height];
+    }
+
+    public void DefineCell(int x, int y, string value)
+    {
+        if (_cells[x,y] != null)
+        {
+            throw new ArgumentException("cell already defined");
+        }
+        for (var c = 0; c < _cells.GetLength(0); c++)
+        {
+            for (var r = 0; r < _cells.GetLength(1); r++)
+            {
+                if (value == _cells[c,r])
+                    throw new ArgumentException(value + " already present at " + c + "," + r);
+            }
+        }
+        _cells[x,y] = value;
+    }
+
+    public void MarkCell(int x, int y)
+    {
+        if (!IsInitialized())
+        {
+            throw new ArgumentException("board not initialized");
+        }
+        _marked[x,y] = true;
+    }
+
+    public bool IsMarked(int x, int y)
+    {
+        return _marked[x,y];
+    }
+
+    public bool IsInitialized()
+    {
+        foreach(var cell in _cells)
+        {
+            if (cell == null)
+                    return false;
+        }
+        return true;
+    }
+
+}
---
+++ csharp/Bingo/BingoTest.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+
+namespace Bingo;
+
+public class BingoTest
+{
+    private BingoBoard? _board;
+
+    [Fact]
+    public void ANewlyCreatedBoardIsNotInitialized()
+    {
+        GivenBingoBoardOfSize(1, 1);
+        ThenBoardIsNotInitialized();
+    }
+
+    [Fact]
+    public void WhenAllFieldsAreSetTheBoardIsInitialized()
+    {
+        var anyValue = "42";
+        GivenBingoBoardOfSize(1, 1);
+        WhenCellIsDefined(0, 0, anyValue);
+        ThenBoardIsInitialized();
+    }
+
+    [Fact]
+    public void WhenAllFieldsOnRectangularBoardAreSetItIsInitialized()
+    {
+        var one = "0, 0";
+        var two = "0, 1";
+        GivenBingoBoardOfSize(1, 2);
+        WhenCellIsDefined(0, 0, one);
+        WhenCellIsDefined(0, 1, two);
+        ThenBoardIsInitialized();
+    }
+
+    [Fact]
+    public void ADefinedCellCantBeRedefinedEvenIfItsTheSameValue()
+    {
+        var anyValue = "42";
+        GivenBingoBoardOfSize(1, 1);
+        WhenCellIsDefined(0, 0, anyValue);
+        var e = Assert.Throws<ArgumentException>(() => WhenCellIsDefined(0, 0, anyValue));
+        Assert.Contains("already defined", e.Message);
+    }
+
+    [Fact]
+    public void DuplicateCellsAreNotAllowed()
+    {
+        var anyValue = "42";
+        GivenBingoBoardOfSize(2, 2);
+        WhenCellIsDefined(0, 1, anyValue);
+
+        var e = Assert.Throws<ArgumentException>(() => WhenCellIsDefined(1, 0, anyValue));
+        Assert.Contains("already present at 0,1", e.Message);
+    }
+
+    [Fact]
+    public void ANonInitializedBoardCannotBeMarked()
+    {
+        GivenBingoBoardOfSize(1, 1);
+        var e = Assert.Throws<ArgumentException>(() => WhenCellIsMarked(0, 0));
+        Assert.Contains("not initialized", e.Message);
+    }
+
+    [Fact]
+    private void WhenAllCellGetsMarkedItIsMarked()
+    {
+        var anyValue = "42";
+        GivenBingoBoardOfSize(1, 1);
+        WhenCellIsDefined(0, 0, anyValue);
+        WhenCellIsMarked(0, 0);
+        ThenCellIsMarked(0, 0);
+    }
+
+    private void GivenBingoBoardOfSize(int width, int height)
+    {
+        _board = new BingoBoard(width, height);
+    }
+
+    private void WhenCellIsDefined(int x, int y, string value)
+    {
+        _board!.DefineCell(x, y, value);
+    }
+
+    private void WhenCellIsMarked(int x, int y)
+    {
+        _board!.MarkCell(x, y);
+    }
+
+    private void ThenBoardIsNotInitialized()
+    {
+        Assert.False(BoardInitializeState());
+    }
+
+    private void ThenBoardIsInitialized()
+    {
+        Assert.True(BoardInitializeState());
+    }
+
+    private bool BoardInitializeState()
+    {
+        return _board!.IsInitialized();
+    }
+
+    private void ThenCellIsMarked(int x, int y)
+    {
+        Assert.True(_board!.IsMarked(x, y));
+    }
+}
